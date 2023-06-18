47d6538c1b61bfc9210e222711d5874583848259
Bruno <brunochelimo@gmail.com>
2023-06-18 16:26:51 +0300

    alarm system

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 
-import './App.css';
+// import './App.css';
+// import LandingPage from './components/LandingPage';
+
+
+
+// import React from 'react'
+
+// function App() {
+//   return (
+//     <div>
+//       <LandingPage/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
-
-
-
-import React from 'react'
+import Home from './components/Home';
 
 function App() {
   return (
-    <div>
-      <LandingPage/>
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
